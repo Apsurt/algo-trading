a7cf8d66ca3e974616b8d8643ed2ac935287c623
Apsurt <tymon@becella.eu>
2025-03-13 22:58:22 +0100

    Combiner framework

--- src/combiner.py
+++ src/combiner.py
@@ -1,5 +1,10 @@
-
+from indicator_combiner import IndicatorCombiner
 
 class Combiner:
     def __init__(self) -> None:
-        pass
+        # Indicators
+        indicator_combiner = IndicatorCombiner()
+
+        # Chart Patterns
+        # ML
+        # ML Sentiment
