c2d823797c655ea6c256e4aff5ddad198c243f43
Apsurt <tymon@becella.eu>
2025-03-13 21:57:41 +0100

    Combiner framework

--- src/indicator_combiner.py
+++ src/indicator_combiner.py
@@ -1,4 +1,19 @@
+from typing import Tuple
+from structs import Indicator, Signal
+
+weights = {
+    Indicator.RSI: 1,
+}
 
 class IndicatorCombiner:
-    def __init__(self) -> None:
+    def __init__(self, weights = weights) -> None:
+        self.weights = weights
+
+    def __call__(self, *args: Tuple[Indicator, float]) -> Signal:
         pass
+
+if __name__ == "__main__":
+    combiner = IndicatorCombiner()
+    indicator_list = [(Indicator.RSI, 0.0)]
+
+    final_signal = combiner(*indicator_list)
