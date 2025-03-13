201e475dc85ba4415eee521b88a2938ae76e2339
Apsurt <tymon@becella.eu>
2025-03-13 22:37:19 +0100

    Type hinting in indicator_combiner.py

--- src/indicator_combiner.py
+++ src/indicator_combiner.py
@@ -1,13 +1,14 @@
-from typing import Tuple
+from typing import Tuple, Dict
 from structs import Indicator, Signal
+from indicators import Indicators
 
 weights = {
-    Indicator.RSI: 1,
+    Indicator.RSI: 1.0,
 }
 
 class IndicatorCombiner:
     def __init__(self, weights = weights) -> None:
-        self.weights = weights
+        self.weights: Dict[Indicator, float] = weights
 
     def __call__(self, *args: Tuple[Indicator, float]) -> Signal:
 
@@ -15,6 +16,6 @@ class IndicatorCombiner:
 
 if __name__ == "__main__":
     combiner = IndicatorCombiner()
-    indicator_list = [(Indicator.RSI, 0.0)]
+    indicators = Indicators()
 
-    final_signal = combiner(*indicator_list)
+    final_signal = combiner(*indicators.)
