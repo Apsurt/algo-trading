a7094a00fc627228d4a57ae6ddbcdd36b1bd9fb2
justMG <bob.vsauce.xt2@gmail.com>
2025-03-13 22:03:36 +0100

    add barebones indicator class

--- src/indicators.py
+++ src/indicators.py
@@ -1,1 +1,11 @@
 # abc
+import structs
+from typing import Tuple
+
+class Indicators:
+    def __init__(self) -> None:
+        pass
+
+    def SMA(self) -> Tuple [structs.Signal, structs.Indicator]:
+
+        return []
